2d340b036d3ba6d10e33b2c56bf1ba322517b96c
ludovicdean <ludovicd58@gmail.com>
2020-10-06 14:46:06 +0200

    TP partie 1

--- trivial-pursuit/src/app/home/home.page.ts
+++ trivial-pursuit/src/app/home/home.page.ts
@@ -6,7 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  pseudo : string = '';
+  difficulte : string ='';
+  error : string = '';
+  isVisible : boolean = false;
+  isVisibleQuestion : boolean = true;
+  isVisibleNextQuestion : boolean = true;
 
   constructor() {}
+
+  public verifierInfos(){
+    
+    if(!this.difficulte && !this.pseudo){
+      this.error = 'Veuillez renseigner votre pseudo et la difficulté choisis';
+      return; 
+    }
+    
+    if(!this.difficulte){
+      this.error = 'Veuillez renseigner le niveau de difficulté choisi !';
+    }
+
+    if(!this.pseudo || this.pseudo.length < 3){
+      this.error = 'Veuillez renseigner un pseudo de plus de 3 caractères';
+      return;
+    }
+    this.isVisible = true;
+    this.isVisibleQuestion = false;
+    
+  }
+
+  public reponse(){
+    this.isVisibleNextQuestion = false;
+  }
 
 }
